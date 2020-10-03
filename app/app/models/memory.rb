528711a1ff22afc8d20f0791cd381f10b4048a80
Edimo Sousa Silva <edimossilva@gmail.com>
2020-10-03 09:12:29 +0300

    6 - add unique validation to memory key (#17)

--- app/app/models/memory.rb
+++ app/app/models/memory.rb
@@ -1,3 +1,4 @@
 class Memory < ApplicationRecord
   validates_presence_of :key, :value
+  validates_uniqueness_of :key, case_sensitive: false
 end
--- app/spec/models/memory_spec.rb
+++ app/spec/models/memory_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Memory, type: :model do
-  it { should validate_presence_of(:key) }
-  it { should validate_presence_of(:value) }
+  describe 'validations' do
+    it { should validate_presence_of(:key) }
+    it { should validate_presence_of(:value) }
+    it { should validate_uniqueness_of(:key).case_insensitive }
+  end
 end
