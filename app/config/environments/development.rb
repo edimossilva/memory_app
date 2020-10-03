ae1d541ce668944d32fa9980b5f732d0e1e713f0
Edimo Sousa Silva <edimossilva@gmail.com>
2020-10-03 22:06:44 +0300

    #27 - deploy to heroku (#28)

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -51,4 +51,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.reload_classes_only_on_change = false
+  config.hosts << "memory-app-back.herokuapp.com"
 end
