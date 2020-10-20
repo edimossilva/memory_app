87e2d46145e3d385e88299fb5f0854577fc59daf
Edimo Sousa Silva <edimossilva@gmail.com>
2020-10-20 22:54:12 +0300

    add login to vue client (#10)
    * add login to vue client
    
    * fix snapshots
    
    * try env to cypress
    
    * add seed
    
    * sync workers
    
    * spliting frontend and integration specs
    
    * ignore on code climate
    
    * try to ignore on climate again

---
+++ frontend_vue/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const HOST = process.env.VUE_APP_API_URL || 'https://memory-app-back.herokuapp.com';
+const LOGIN_URL = `${HOST}/auth/login`;
+
+export const doLoginApi = (username, password) => {
+  const params = { username, password };
+
+  return axios.post(LOGIN_URL, params);
+};
