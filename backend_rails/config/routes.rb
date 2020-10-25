337b6cd86a721816d3d50cc2fee526481a480ece
edimo sousa <edimossilva@gmail.com>
2020-10-25 18:28:17 +0200

    generate devise initial config

--- backend_rails/config/routes.rb
+++ backend_rails/config/routes.rb
@@ -3,9 +3,12 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  root to: "authentication#sign_up"
+
   post '/auth/login' => 'authentication#login'
   get '/auth/sign_up' => 'authentication#sign_up'
   get '/auth/check' => 'authentication#check'
   get '/auth/:provider/callback' => 'sessions#omniauth'
+
   resources :memories
 end
