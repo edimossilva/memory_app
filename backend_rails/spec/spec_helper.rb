173d7f48b7019f1ff0c6a219532f152d0ecf9b20
edimo sousa <edimossilva@gmail.com>
2020-10-30 20:49:50 +0200

    config capybara

--- backend_rails/spec/spec_helper.rb
+++ backend_rails/spec/spec_helper.rb
@@ -47,16 +47,29 @@ end
 
 # config capybara begin
 require "selenium/webdriver"
-Capybara.javascript_driver = :selenium_chrome_headless
-Capybara.server_port = 30000
+Capybara.server = :puma, { Silent: true }
 
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
+
+# Setup rspec
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-		driven_by(:rack_test)
-	end
+    driven_by(:rack_test)
+  end
 
-	config.before(:each, type: :system, js: true) do
-		driven_by(:selenium_chrome_headless)
+  config.before(:each, type: :system, js: true) do
+    driven_by(:chrome_headless)
   end
 end
 # config capybara end
