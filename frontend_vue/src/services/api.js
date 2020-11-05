e21c11da9bd3d9f9504539b00b0f3672139a8344
edimossilva <edimossilva@gmail.com>
2020-11-05 22:13:52 +0200

    auto logout when invalid token

--- frontend_vue/src/services/api.js
+++ frontend_vue/src/services/api.js
@@ -1,13 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 
 const HOST =
-  process.env.VUE_APP_API_URL || "https://memory-app-back.herokuapp.com";
+  process.env.VUE_APP_API_URL || 'https://memory-app-back.herokuapp.com';
 
 const LOGIN_URL = `${HOST}/auth/login`;
+const CHECK_TOKEN_URL = `${HOST}/auth/check`;
 export const OMNIAUTH_URL = `${HOST}/auth/sign_up`;
+
+export const getRequestConfig = () => {
+  const token = localStorage.getItem('token');
+  const requestConfig = {
+    headers: {
+      Authorization: `Basic ${token}`,
+    },
+  };
+
+  return requestConfig;
+};
 
 export const doLoginApi = (username, password) => {
   const params = { username, password };
 
   return axios.post(LOGIN_URL, params);
+};
+
+export const checkAuthApi = () => {
+  return axios.get(CHECK_TOKEN_URL, getRequestConfig());
 };
