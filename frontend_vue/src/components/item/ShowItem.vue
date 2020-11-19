e6be0c23f09c196f2c359ed3a84829b343d7f830
Edimo Sousa Silva <edimossilva@gmail.com>
2020-11-20 00:18:48 +0200

    Improve layout (#57)
    * cards with same height
    
    * fix show-item alignment
    
    * fix specs

--- frontend_vue/src/components/item/ShowItem.vue
+++ frontend_vue/src/components/item/ShowItem.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <div class="card" :data-cy="`show_item__card_${item.key}`">
-      <div class="card-content">
-        <div class="content">{{ item.key }}: {{ item.value }}</div>
+  <div class="h-100">
+    <div
+      class="card bm--card-equal-height"
+      :data-cy="`show_item__card_${item.key}`"
+    >
+      <header class="card-header">
+        <p class="card-header-title is-centered">{{ item.key }}</p>
+      </header>
+      <div class="card-content h-100">
+        <div class="content h-100">{{ item.value }}</div>
         <div class="tags is-centered">
           <span
             v-for="tag in item.tags"
@@ -122,3 +128,23 @@ export default {
   },
 };
 </script>
+<style lang="css" scoped>
+.h-100 {
+  height: 100%;
+}
+.bm--card-equal-height {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.bm--card-equal-height .card-footer {
+  margin-top: auto;
+}
+.card-content {
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  width: 100%;
+}
+</style>
