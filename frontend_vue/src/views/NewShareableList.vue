3a4cc7fc127ae82c98f18d6602f9535fb8cdbdc8
edimo sousa <edimossilva@gmail.com>
2020-11-30 23:49:42 +0200

    formating shareable lists

--- frontend_vue/src/views/NewShareableList.vue
+++ frontend_vue/src/views/NewShareableList.vue
@@ -1,43 +1,82 @@
 <template>
-  <div data-cy="list_shareable_list">
-    <div class="columns m-6">
-      <div class="column is-6 notification is-info mr-4">
-        <draggable
-          class="list-group columns is-multiline"
-          :list="allItems"
-          group="item"
-          @change="log"
-          v-bind:class="{
-            'new-shareable-list__items_empty': allItems.length === 0,
-          }"
-        >
-          <div
-            class="list-group-item column is-half"
-            v-for="item in items"
-            :key="item.key"
-          >
-            <draggable-item :item="item"></draggable-item>
+  <div>
+    <div class="columns mx-6 mt-2">
+      <div class="column is-6">
+        <div class="columns is-multiline">
+          <div class="column mt-6 mb-1"></div>
+          <div class="column is-12 notification is-info mr-4 mb-0">
+            <h2 class="is-size-3 notification p-1 is-primary">All Items</h2>
+            <draggable
+              class="list-group columns is-multiline"
+              :list="allItems"
+              group="item"
+              @change="log"
+              v-bind:class="{
+                'new-shareable-list__items_empty': allItems.length === 0,
+              }"
+            >
+              <div
+                class="list-group-item column is-half"
+                v-for="item in items"
+                :key="item.key"
+              >
+                <draggable-item :item="item"></draggable-item>
+              </div>
+            </draggable>
           </div>
-        </draggable>
+        </div>
       </div>
-      <div class="column is-6 notification is-info ml-4 mb-5 h-auto">
-        <draggable
-          class="list-group columns is-multiline"
-          :list="shareableList.items"
-          group="item"
-          @change="log"
-          v-bind:class="{
-            'new-shareable-list__items_empty': shareableList.items.length === 0,
-          }"
-        >
-          <div
-            class="list-group-item column is-half"
-            v-for="item in shareableList.items"
-            :key="item.key"
-          >
-            <draggable-item :item="item"></draggable-item>
+      <div class="column is-6 notification is-secondary ml-4 pb-0">
+        <div class="columns is-multiline">
+          <div class="column is-12">
+            <b-field label="Name" label-position="inside" type="is-primary">
+              <b-input
+                type="text"
+                v-model="shareableList.name"
+                data-cy="new-shareable-list__name_input"
+              ></b-input>
+            </b-field>
           </div>
-        </draggable>
+        </div>
+        <div class="columns is-multiline mb-0">
+          <div class="column notification is-info mx-2 mb-2 h-auto">
+            <h2 class="is-size-3 notification p-1 is-primary">Items</h2>
+            <draggable
+              class="list-group columns is-multiline"
+              :list="shareableList.items"
+              group="item"
+              @change="log"
+              v-bind:class="{
+                'new-shareable-list__items_empty':
+                  shareableList.items.length === 0,
+              }"
+            >
+              <div
+                class="list-group-item column is-half"
+                v-for="item in shareableList.items"
+                :key="item.key"
+              >
+                <draggable-item :item="item"></draggable-item>
+              </div>
+            </draggable>
+          </div>
+        </div>
+        <div class="columns is-multiline mb-0">
+          <div class="column mx-2 mb-0 h-auto">
+            <b-field>
+              <b-button
+                class="m-2"
+                @click="onConfirm()"
+                type="is-primary"
+                data-cy="item_modal__confirm_button"
+                icon-left="plus"
+              >
+                Create list
+              </b-button>
+            </b-field>
+          </div>
+        </div>
+        <div class="new-shareable-list__button-container"></div>
       </div>
     </div>
   </div>
@@ -97,5 +136,16 @@ export default {
 }
 .new-shareable-list__items_empty {
   height: 100%;
+}
+.new-shareable-list__selected_items {
+  height: 100%;
+}
+.new-shareable-list__name_column {
+  height: 100%;
+}
+.new-shareable-list__button-container {
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
 }
 </style>
