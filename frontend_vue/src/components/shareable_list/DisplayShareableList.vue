699acbfe6c438d9be53e3a6c9d6fcefc723e5c55
edimo sousa <edimossilva@gmail.com>
2020-12-20 20:41:15 +0200

    enable public sharable list

--- frontend_vue/src/components/shareable_list/DisplayShareableList.vue
+++ frontend_vue/src/components/shareable_list/DisplayShareableList.vue
@@ -5,8 +5,14 @@
       :external-items="shareableList.items"
       class="m-4"
     >
-      <div class="notification is-secondary mb-6 p-2">
+      <div class="notification is-secondary mb-5 p-2">
         <h1 class="is-size-3">{{ this.shareableList.name }}</h1>
+      </div>
+
+      <div class="notification is-secondary mb-6 p-2">
+        <a class="is-size-5" :href="this.shareableListUrl">{{
+          this.shareableListUrl
+        }}</a>
       </div>
     </list-items>
   </div>
@@ -14,7 +20,10 @@
 
 <script>
 import ListItems from "@/components/item/ListItems.vue";
-import { getShareableListApi } from "../../services/shareableListsApi";
+import {
+  getShareableListApi,
+  getPublicShareableListApi,
+} from "../../services/shareableListsApi";
 
 export default {
   name: "DisplayShareableItem",
@@ -22,12 +31,14 @@ export default {
   mounted() {
     this.id = this.$route.params.id;
     if (this.id) {
-      getShareableListApi(this.id).then(
-        (response) => {
-          this.shareableList = response.data.data;
-          this.shareableList.items = this.shareableList.memories;
-          delete this.shareableList.memories;
-        },
+      getShareableListApi(this.id).then(this.setShareableList, (error) => {
+        console.log(error);
+      });
+    } else {
+      const username = this.$route.params.username;
+      const shareableListName = this.$route.params.shareableListName;
+      getPublicShareableListApi(username, shareableListName).then(
+        this.setShareableList,
         (error) => {
           console.log(error);
         }
@@ -38,7 +49,27 @@ export default {
     return {
       shareableList: { name: "", items: [] },
       id: null,
+      isPublic: false,
     };
+  },
+  computed: {
+    shareableListUrl() {
+      if (this.isPublic) {
+        return window.location.href;
+      }
+      const username = localStorage.username;
+      const shareableListName = this.shareableList.name;
+      const host = window.location.href.split("shareable_list")[0];
+      return `${host}${username}/${shareableListName}`;
+    },
+  },
+  methods: {
+    setShareableList(response) {
+      this.isPublic = true;
+      this.shareableList = response.data.data;
+      this.shareableList.items = this.shareableList.memories;
+      delete this.shareableList.memories;
+    },
   },
 };
 </script>
